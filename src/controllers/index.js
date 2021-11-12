5ec9f85fddf654fe067082c28fdb271f516f7f26
Theo Silva <theodorotheodore@gmail.com>
2021-11-12 00:43:47 -0300

    Now our application already can update news in database

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,8 +3,10 @@ const News = require('../models/News');
 module.exports = {
     async getNews (req, res) {
         const news = await News.findAll()
+
         res.render('./index.ejs', { pageTitle: 'Home - News CRUD', news: news });
     },
+
     async readNews (req, res) {
         const id = req.query.id;
         const news = await News.findOne({
@@ -12,14 +14,55 @@ module.exports = {
                 id: id
             }
         })
-        res.render('./noticia.ejs', { pageTitle: news.title, news: news });
+
+        res.render('./noticia.ejs', { pageTitle: `${news.title} - News CRUD`, news: news });
     },
+
     async getCreateNews (req, res) {
         res.render('./cadastrar.ejs', { pageTitle: 'Criação - News CRUD' });
     },
+
     async postCreateNews (req, res) {
         let news = req.body;
         news = await News.create(news);
+
         return res.redirect('/');
     },
+
+    async getDeleteNews (req, res) {
+        const id = req.query.id;
+        const news = await News.destroy({
+            where: {
+                id: id
+            }
+        });
+
+        return res.redirect('/');        
+    },
+
+    async getUpdateNews (req, res) {
+        const id = req.query.id;
+        let news = await News.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        return res.render('./atualizar.ejs', { pageTitle: `${news.title} - News CRUD`, news: news }); 
+    },
+
+    async postUpdateNews (req, res) {
+        const id = req.query.id
+        let news = req.body;
+        news = await News.update(
+            news, 
+            {
+                where: {
+                    id: id
+                }
+            }
+        );
+
+        return res.redirect('/');
+    }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,15 @@ routes.use(express.json());
 routes.use(express.urlencoded({ extended: true }));
 
 routes.get('/', controllers.getNews);
+
 routes.get('/news', controllers.readNews);
+
 routes.get('/create_news', controllers.getCreateNews);
 routes.post('/create_news', controllers.postCreateNews);
+
+routes.get('/delete_news', controllers.getDeleteNews);
+
+routes.get('/update_news', controllers.getUpdateNews);
+routes.post('/update_news', controllers.postUpdateNews);
 
 module.exports = routes;
